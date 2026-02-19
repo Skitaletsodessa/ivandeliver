5657ed840e5d3327d9dd2a1ce6b5a7f01fa45150
Skitaletsodessa <skitalets.od@gmail.com>
2026-02-19 14:26:22 +0100

    Spotify cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,15 @@
-// @ts-check
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://ivandeliver.email',
-  
-  // Режим сервера для API (Spotify)
   output: 'server',
 
   adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-    // Используем встроенный сервис Cloudflare для картинок
+    platformProxy: { enabled: true },
     imageService: 'cloudflare',
-    // Отключаем поиск SESSION KV, чтобы убрать лишние ворнинги в логах
     persistedStoredValues: false 
   }),
   
@@ -27,16 +19,6 @@ export default defineConfig({
   ],
 
   build: {
-    // Решает проблему "Render blocking requests": инлайнит CSS прямо в HTML, 
-    // так как файл стилей у тебя крошечный. Это ускорит LCP.
     inlineStylesheets: 'always'
-  },
-  
-  env: {
-    schema: {
-      CLIENT_ID: envField.string({ context: "server", access: "secret" }),
-      CLIENT_SECRET: envField.string({ context: "server", access: "secret" }),
-      REFRESH_TOKEN: envField.string({ context: "server", access: "secret" }),
-    },
-  },
+  }
 });
